02c3aa742c42b68957fd27f582c145a07614c57b
Anton Piatek <anton@piatek.co.uk>
2015-05-17 16:14:28 +0100

    set light pins and timeouts

--- ToyKitchenWiring.ino
+++ ToyKitchenWiring.ino
@@ -1,63 +1,28 @@
+#include "LightController.h"
+#include "config.h"
+// edit above config.h, set DEBUG #define for debug output on serial
+
 const int ledPins[3] = {11,12,13};
-int ledStates[3]     = {LOW,LOW,LOW};
 const int buttonPins[3] = {2,3,4};
 
-int pressed[3]={0};
-int readCount[3] = {0,0,0};
-unsigned long onTime[3] = {0};
+LightController l1 = LightController(ledPins[2], buttonPins[0],"1");
+LightController l2 = LightController(ledPins[1], buttonPins[1],"2");
+LightController l3 = LightController(ledPins[0], buttonPins[2],"3");
 
 void setup() {
-  // Setup the buttons with an internal pull-up and debouncer
-  for (int i = 0; i < sizeof(buttonPins); i++){
-    pinMode(buttonPins[i],INPUT);
-  }
-
-  // Set leds
-  for (int i = 0; i < sizeof(ledPins); i++){
-    pinMode(ledPins[i], OUTPUT);
-    digitalWrite(ledPins[i], ledStates[i]);
-  }
+  l1.setTimeout(3);
+  l2.setTimeout(3);
+  l3.setTimeout(120);
+  #ifdef DEBUG
   Serial.begin(9600); 
+  Serial.println("Run :)");
+  #endif
 }
 
-#define AUTO_OFF_TIME 30
 
 void loop() {
-  for (int i = 0; i < 3; i++){
-    //auto turn off
-    if(ledStates[i] == HIGH && readCount[i] ==0 && pressed[i] == 0){
-      unsigned long offTime = onTime[i] + ((unsigned long)AUTO_OFF_TIME * 1000);
-      unsigned long now = millis();
-      if( offTime < now ){
-        Serial.println("AUTOOFF: "+String(i)+" "+String(ledStates[i])+" OnTime: "+String(onTime[i])+" OffTime: "+String(offTime)+" now:"+String(now));
-        ledStates[i] = LOW;
-        digitalWrite(ledPins[i], ledStates[i] );
-      }
-    } 
-
-    //check for button presses
-    int reading = digitalRead(buttonPins[i]);
-    //debounce
-    if ( reading == HIGH ){
-      if(readCount[i] < 100){
-        readCount[i]++;
-      }
-    }else if(readCount[i] > 0){
-      readCount[i]--;
-    }else if (readCount[i] == 0){
-      pressed[i]=0;
-    }
-    //turn on if sufficiently debounced
-    if(pressed[i] == 0 && readCount[i] >= 50 ){
-      unsigned long now = millis();
-      Serial.println("ON: butt:"+String(i)+" state:"+String(ledStates[i])+" pressed:"+String(pressed[i])+" count:"+String(readCount[i])+" now:"+String(now));
-      pressed[i]=1;
-      ledStates[i] = !ledStates[i];
-      digitalWrite(ledPins[i], ledStates[i] );
-      if(ledStates[i] == HIGH) {
-        onTime[i] = now;
-      }
-      //Serial.println(readCount[i]);
-    }
-  }
+  l1.checkButton();
+  l2.checkButton();
+  l3.checkButton();
 }
+
