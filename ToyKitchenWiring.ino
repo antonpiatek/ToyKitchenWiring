3614a65e59992bf647e42e440a72e2c8aeb420a0
Anton Piatek <anton@piatek.co.uk>
2015-05-23 20:07:11 +0100

    move to nano wiring

--- ToyKitchenWiring.ino
+++ ToyKitchenWiring.ino
@@ -2,17 +2,17 @@
 #include "config.h"
 // edit above config.h, set DEBUG #define for debug output on serial
 
-const int ledPins[3] = {11,12,13};
+const int ledPins[3] = {12,10,11};
 const int buttonPins[3] = {2,3,4};
 
-LightController l1 = LightController(ledPins[2], buttonPins[0],"1");
-LightController l2 = LightController(ledPins[1], buttonPins[1],"2");
-LightController l3 = LightController(ledPins[0], buttonPins[2],"3");
+LightController l0 = LightController(ledPins[0], buttonPins[0],"0");
+LightController l1 = LightController(ledPins[1], buttonPins[1],"1");
+LightController l2 = LightController(ledPins[2], buttonPins[2],"2");
 
 void setup() {
+  l0.setTimeout(15);
   l1.setTimeout(15);
-  l2.setTimeout(15);
-  l3.setTimeout(120);
+  l2.setTimeout(120);
   #ifdef DEBUG
   Serial.begin(9600); 
   Serial.println("Run :)");
@@ -21,8 +21,8 @@ void setup() {
 
 
 void loop() {
+  l0.checkButton();
   l1.checkButton();
   l2.checkButton();
-  l3.checkButton();
 }
 
